2ab86e3a2b820e3087d2f593a6390023d50ebc79
cvaibhavi <cvaibhavi2001@gmail.com>
2022-09-17 11:29:56 +0530

    changed resume

--- src/Pages/Sports.js
+++ src/Pages/Sports.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Icon, IconButton, Spacer, Text } from '@chakra-ui/react'
+import {  Flex, Icon, IconButton, Spacer, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import { FaArrowLeft, FaAward, FaCamera } from 'react-icons/fa'
 import { useMediaQuery } from '@chakra-ui/media-query';
@@ -10,15 +10,14 @@ const Sports = () => {
     const navigate = useNavigate(); 
 
   return (
-    <Flex direction={isNotSmallerScreen ? "row" : "column"} w="100%" 
-            maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}>
-            <Box alignSelf="center" px="32" py="16">
+    <Stack alignItems='center' mb='8'>
             <Link to="/">
               <IconButton ml={2} icon={<FaArrowLeft />} isRound='true'/>  
             </Link>
-            <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8} >
+         <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8} >
+          
             
-            <div onClick={() => navigate("/achievements")} p='5px'>  
+            <div onClick={() => navigate("/achievements")}>  
                 
             <Flex rounded="xl" direction="column" mt={4} 
                           bg="gray.100" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "green.400", }}
@@ -44,9 +43,7 @@ const Sports = () => {
             </div><br/>
             </Flex>
             
-        </Box>
-        
-    </Flex>
+     </Stack>
     
   )
 }
